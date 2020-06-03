219b9d7090a5fc678816ba2c19fb46b8c681034d
Jalomo1197 <alexisjalomo1197@gmail.com>
2020-06-03 15:32:15 -0500

    Updates

---
+++ precache-manifest.ef832f42ece57054080866bff0346f8e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1e4409ae0c25241a2f07f421ceb4bf7",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "0aaf1d4147ac7eebc822",
+    "url": "/portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d3b3b7e629bf9bd4d3c8",
+    "url": "/portfolio/static/js/2.df5d811d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.df5d811d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0aaf1d4147ac7eebc822",
+    "url": "/portfolio/static/js/main.dd9eb82d.chunk.js"
+  },
+  {
+    "revision": "f44e0a1de5bb428e3a0e",
+    "url": "/portfolio/static/js/runtime-main.8c03e37b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
