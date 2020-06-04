846f46f949e705a9e207f9cbe947523f1986564f
Jalomo1197 <alexisjalomo1197@gmail.com>
2020-06-04 14:50:46 -0500

    Nav bar added

--- src/App.js
+++ src/App.js
@@ -1,29 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
+import name from './Name.gif'
 import { motion } from "framer-motion"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <motion.circle
-        animate={{ rotate: 360 }}
-        transition={{ duration: 2 }}
-      />
+      <div className="navigation">
+        <img src={name} className="logo" alt="Logo Image" />
+        <div className="navigation-sub">
+          <a href="" className="item">Projects</a>
+          <a href="" className="item">Articles</a>
+          <a href="" className="item">About</a>
+        </div>
+      </div>
     </div>
   );
 }
