f01e2e53cd4e03e0149619ece7489ffbb2b5d844
Jalomo1197 <alexisjalomo1197@gmail.com>
2020-06-04 15:17:23 -0500

    background

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import React from 'react';
 import logo from './logo.svg';
 import name from './Name.gif'
 import { motion } from "framer-motion"
+import {useSpring, animated} from 'react-spring'
 import './App.css';
 
 function App() {
+  const props = useSpring({opacity: 1, from: {opacity: 0}})
+
   return (
     <div className="App">
       <div className="navigation">
+      <animated.div style={props}>
         <img src={name} className="logo" alt="Logo Image" />
+        </animated.div>
         <div className="navigation-sub">
           <a href="" className="item">Projects</a>
           <a href="" className="item">Articles</a>
