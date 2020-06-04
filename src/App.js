f736c7453b0b2a9bb1b7aeb139e2126a7bd17ffe
Jalomo1197 <alexisjalomo1197@gmail.com>
2020-06-04 15:41:43 -0500

    background

--- src/App.js
+++ src/App.js
@@ -3,7 +3,20 @@ import logo from './logo.svg';
 import name from './Name.gif'
 import { motion } from "framer-motion"
 import {useSpring, animated} from 'react-spring'
+import styled from 'styled-components'
 import './App.css';
+
+
+const Button = styled.button`
+  background: transparent;
+  border-radius: 3px;
+  border: 2px solid palevioletred;
+  color: palevioletred;
+  margin: 0 1em;
+  padding: 0.25em 1em;
+`
+
+
 
 function App() {
   const props = useSpring({opacity: 0.5, from: {opacity: 0}})
@@ -16,8 +29,7 @@ function App() {
       <div className="navigation">
       
         <img src={name} className="logo" alt="Logo Image" />
-      
-        <AnimatedDonut percent={props2.value} />
+    
         <div className="navigation-sub">
           <a href="" className="item">Projects</a>
           <a href="" className="item">Articles</a>
@@ -25,6 +37,7 @@ function App() {
         </div>
       </div>
       <animated.div style={props}> I will FADE </animated.div>
+      <Button> Projects </Button>
     </div>
   );
 }
